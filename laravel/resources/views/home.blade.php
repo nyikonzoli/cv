d896c3551dce38918dc43fe32dd53d788f967256
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-03 18:23:48 +0100

    profile base

---
+++ laravel/resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Zoltán Nyikon</title>
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css'); }}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+    <section id="profile">
+        <div id="profile-content">
+            <div id="profile-picture">
+                <img src="https://dummyimage.com/450x450/fa7/268" alt="me">
+            </div>
+            <div id="profile-text">
+                <h1><b>Hello</b></h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione velit, hic beatae tempora earum nulla accusamus saepe tempore vero est adipisci ad obcaecati veniam dolores exercitationem odio aliquid voluptate fugiat.</p>                
+            </div>
+        </div>
+    </section>
+    <section>
+        
+    </section>
+</body>
+</html>
