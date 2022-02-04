7817b4e0353389d1e74f1f25bb8c11f4237222ea
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-04 18:25:14 +0100

    layers

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -6,14 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Zoltán Nyikon</title>
-    <link rel="stylesheet" href="{{ URL::asset('css/app.css'); }}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body>
     <section id="profile">
         <div id="profile-content">
             <div id="profile-picture">
-                <img src="https://dummyimage.com/450x450/fa7/268" alt="me">
+                <img src="https://dummyimage.com/450x450/ff8/c64" alt="me">
             </div>
             <div id="profile-text">
                 <h1><b>Hello</b></h1>
@@ -21,8 +20,19 @@
             </div>
         </div>
     </section>
-    <section>
-        
+    <div class="spacer" style="background-image: url({{asset('svg/spacer1.svg')}});"></div>
+    <section id="resume">
+        <div id="resume-content">
+           <div class="resume-card">
+
+           </div>
+        </div>
+    </section>
+    <div class="spacer" style="background-image: url({{asset('svg/spacer2.svg')}}); height: 150px;"></div>
+    <section id="contact">
+        <div id="contact-content">
+
+        </div>
     </section>
 </body>
 </html>
