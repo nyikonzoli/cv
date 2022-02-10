7a50b2073b15ceea58bc65f1070aad9238a0b591
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-10 21:21:12 +0100

    contact icons

--- laravel/resources/views/contact.blade.php
+++ laravel/resources/views/contact.blade.php
@@ -7,5 +7,53 @@
 @section('title', 'Zoltán Nyikon - Contact')
 
 @section('content')
+    <div id="content">
+        <h1>I'm looking forward to hear from you!</h1>
+        <div>
 
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <div class="info-card">
+                        <div class="circle">
+                            <i class="fa fa-linkedin-square icon"></i>
+                        </div>
+                        <div class="info">
+                            <h5>Check out my LinkedIn porfile!</h5>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                <div class="info-card">
+                        <div class="circle">
+                            <i class="fa fa-github-square icon"></i>
+                        </div>
+                        <div class="info">
+                            <h5>Take a look at my projects!</h5>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                <div class="info-card">
+                        <div class="circle">
+                            <i class="fa fa-phone"></i>
+                        </div>
+                        <div class="info">
+                            <h5>06308446803</h5>
+                        </div>
+                    </div>
+                </li>
+                <li>             
+                    <div class="info-card">
+                        <div class="circle">
+                            <i class="fa fa-envelope"></i>
+                        </div>
+                        <div class="info">
+                            <h5>nyikon.zoli@gmail.com</h5>
+                        </div>
+                    </div></li>
+            </ul>
+        </div>
+    </div>
 @endsection
