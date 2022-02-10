57ad94a65b563a2523d1d447a57c2ba0090217fd
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-10 19:24:56 +0100

    projects

---
+++ laravel/resources/views/projects.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.base')
+
+@section('head')
+    <link rel="stylesheet" href="{{asset('css/projects.css')}}">
+@endsection
+
+@section('title', 'Zoltán Nyikon - Projects')
+
+@section('content')
+
+@endsection
