2fa7c9c4a743003a4e2afeba3b0a44761218287c
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-13 16:00:25 +0100

    card base

---
+++ laravel/public/js/projects/cards.js
@@ -0,0 +1,25 @@
+const projectTitles = [
+    "Webshop",
+    "Exam work",
+    "This CV",
+    "Neural net",
+    "Railway network",
+    "Countless small school projects",
+]
+
+const projectStatuses = [
+    "Ongoing",
+    "Ongoing",
+    "Finished",
+    "Ongoing",
+    "Finished",
+    "Finished",
+];
+
+const projectDescriptions = [
+    "This was a school project in early 2022 and we were working on it for about one and a half months. We created this project in a team of two and our task was to create a fully functional webshop, with an admin interface. The topic we choose is the webshop of an instrument shop.",
+    "It is the final task that we have to do in our training. We had to come up with a somewhat unique idea. We could also choose the technologies that we wanted to use for this project. Currently, we are working on a site like Etsy.com but with some twists and unique ideas.",
+    "I created this CV because I wanted to gain some more experience besides school, so hopefully I can start working in the industry much more easily. I had a lot of fun building this site and I learned a lot from it.",
+    "I was intrested in artifical intelligence since I was like 14. I already used some libraries in python but I wanted to have a deeper understanding of this technology so I thought it would be a great idea to code my own neural net from scretch.",
+    "This was an other school project"
+];
--- laravel/resources/views/projects.blade.php
+++ laravel/resources/views/projects.blade.php
@@ -7,17 +7,39 @@
 @section('title', 'Zoltán Nyikon - Projects')
 
 @section('content')
-    <div id="main">
-        <div id="title-div">
-            <h1>MY</h1>
-            <h1 class="coloredh1">PROJECTS</h1>
-        </div>
-        <div id="content">
-            <div id="description">
-
+    <div id="content">
+        <div id="all">
+            <div id="title-div">
+                <h1>MY</h1>
+                <h1 class="coloredh1">PROJECTS</h1>
             </div>
-            <div id="cards">
-                
+            <div id="content">
+                <div id="description">
+
+                </div>
+                <div id="cards">
+                    <div class="card-body">
+                        <div class="card-content">
+                            <h4>Projekt neve</h4>
+                            <h6>Projekt státusza</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius porro excepturi quasi et? Autem deleniti alias reprehenderit officiis eaque quibusdam, distinctio eveniet placeat. Laborum voluptates saepe neque inventore! Eum, necessitatibus!</p>
+                            <div class="tags">
+                                <h5><span class="badge badge-secondary">Badge</span></h5>
+                                <h5><span class="badge badge-secondary">Badge</span></h5>
+                                <h5><span class="badge badge-secondary">Badge</span></h5>
+                                <h5><span class="badge badge-secondary">Badge</span></h5>
+                                <h5><span class="badge badge-secondary">Badge</span></h5>
+                                <h5><span class="badge badge-secondary">Badge</span></h5>
+                                <h5><span class="badge badge-secondary">Badge</span></h5>
+                                <h5><span class="badge badge-secondary">Badge</span></h5>
+                            </div>
+                            <button class="btn btn-success">Check out</button>
+                        </div>
+                        <div class="card-image">
+                            <img src="" alt="">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
